5737d87e106c4c9dd1b9e824ed4c2c8c609e71fb
Ezequiel-valdiviezo <154545568+Ezequiel-valdiviezo@users.noreply.github.com>
2025-06-05 15:54:33 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,3 @@
 self.addEventListener('fetch', function(event) {
-   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('Service Worker registrado'))
-      .catch(err => console.error('Error en SW:', err));
-  }
+  // No hace nada especial por ahora
 });
